c6b987e3544305733d1cd7daee645608c4259a45
fred <popvlous@gmail.com>
2022-01-26 15:23:00 +0800

    1.新增reload頁面log

--- app/mail/routes.py
+++ app/mail/routes.py
@@ -180,7 +180,11 @@ def lan():
 @blueprint.route('/mail/reload', methods=['POST', 'GET'])
 def reload():
     mail_id = request.args.get('mail_id')
+    if not mail_id:
+        message = '郵件id遺失，請重新上傳'
+        render_template('mail/mail500.html', message=message, mail_id=mail_id)
     current_language = request.accept_languages.best
     session['language'] = current_language
+    current_app.logger.error(f' 解密郵件頁面重新載入 ')
     return redirect(request.host_url + "portal/" + current_language +"/mail/uploadfile?id=" + mail_id)
 
